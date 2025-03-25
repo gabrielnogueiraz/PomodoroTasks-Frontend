56e81790f1eaecf54912e023eaff1c4804e6ec11
gabrielnogueiraz <gabriel.nogueira00810@gmail.com>
2025-03-25 18:34:31 -0300

    Adiciona roteamento e suporte a arrastar e soltar no aplicativo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import Dashboard from './pages/Dashboard/Dashboard';
+import Tasks from './pages/Tasks/Tasks';
+import Navbar from './components/Navbar/Navbar';
 import './styles/global.css';
 
 function App() {
   return (
-    <div className="app">
-      <Dashboard />
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <Router>
+        <div className="app">
+          <Navbar />
+          <div className="content">
+            <Routes>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/tasks" element={<Tasks />} />
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+            </Routes>
+          </div>
+        </div>
+      </Router>
+    </DndProvider>
   );
 }
 
