e7143b7b8b1e5044aac33c5122944fb21c20c56d
gabrielnogueiraz <gabriel.nogueira00810@gmail.com>
2025-04-03 17:36:54 -0300

    Adiciona barra de navegação para a aplicação

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import styles from './Navbar.module.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "./Navbar.module.css";
+import logo from "../../assets/logo.png"; 
 
 const Navbar: React.FC = () => {
   return (
     <nav className={styles.navbar}>
       <div className={styles.logo}>
-        <h1>PomodoroTasks</h1>
+        <NavLink to="/">
+          <img src={logo} alt="PomodoroTasks" className={styles.logoImage} />
+        </NavLink>
       </div>
       <ul className={styles.navItems}>
         <li>
-          <NavLink 
-            to="/dashboard" 
-            className={({ isActive }) => isActive ? styles.activeLink : ''}
+          <NavLink
+            to="/dashboard"
+            className={({ isActive }) => (isActive ? styles.activeLink : "")}
           >
             Dashboard
           </NavLink>
         </li>
         <li>
-          <NavLink 
-            to="/tasks" 
-            className={({ isActive }) => isActive ? styles.activeLink : ''}
+          <NavLink
+            to="/tasks"
+            className={({ isActive }) => (isActive ? styles.activeLink : "")}
           >
             Quadro de Tarefas
           </NavLink>
